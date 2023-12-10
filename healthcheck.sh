e767dd0c5a2bd0afc2d7d3503fbcac861eaf8990
Noah Overcash <noah@overca.sh>
2023-12-09 20:28:42 -0500

    fix typo

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-curl --silent --fail http://localhost:8080/ready -H "Host: localhost" | grep ok || exit 1
-curl --silent --fail http://localhost:8080/fpm-ping -H "Host: localhost" | grep pong || exit 1
+curl --silent --fail http://localhost:8080/ready -H "Host: internal-status" | grep ok || exit 1
+curl --silent --fail http://localhost:8080/fpm-ping -H "Host: internal-status" | grep pong || exit 1
